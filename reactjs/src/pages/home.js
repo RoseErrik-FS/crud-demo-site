9efd69ce4e7e3f00aaa47ac5d6d9a280a0d03b63
Errik Rose <errose@student.fullsail.edu>
2023-07-09 13:40:43 -0500

    small changes

--- reactjs/src/pages/home.js
+++ reactjs/src/pages/home.js
@@ -3,7 +3,10 @@ import "../App.css";
 function Home() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h3>The Home Page Goes here.</h3>
+        <p>All Navigation is on the top of the page.</p>
+      </header>
     </div>
   );
 }
