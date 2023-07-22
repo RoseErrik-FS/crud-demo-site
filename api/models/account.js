42fb54478ac05362085c6d092692793e4aa06c69
Errik Rose <errose@student.fullsail.edu>
2023-07-22 18:27:36 -0500

    added a model for accounts

---
+++ api/models/account.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const AccountSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const Account = mongoose.model("Account", AccountSchema);
+
+module.exports = Account;
